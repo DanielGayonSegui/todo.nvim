6e852c2f0616c25db2a02cffdc1b373a0ce904fc
danielgayon <48623250+DanielGayonSegui@users.noreply.github.com>
2025-06-16 01:21:57 +0200

    Update init.lua

--- lua/todo/init.lua
+++ lua/todo/init.lua
@@ -1,4 +1,11 @@
-local M = {}
+M = {}
+
+local win = nil
+
+local default_opts = {
+	target_file = "~/notes/todo.md",
+	border = "rounded",
+}
 
 local function expand_path(path)
   if path:sub(1, 1) == "~" then
@@ -7,7 +14,7 @@ local function expand_path(path)
   return path
 end
 
-local function win_config()
+local function win_config(opts)
   local width = math.min(math.floor(vim.o.columns * 0.8), 80)
   local height = math.floor(vim.o.lines * 0.8)
   return {
@@ -16,15 +23,21 @@ local function win_config()
     height = height,
     col = math.floor((vim.o.columns - width) / 2),
     row = math.floor((vim.o.lines - height) / 2),
-    border = "rounded",
+    border = opts.border,
   }
 end
 
-local function open_floating_file(target_file)
-  local expanded_path = expand_path(target_file)
+local function open_floating_file(opts)
+  if win ~= nil and vim.api.nvim_win_is_valid(win) then
+		vim.api.nvim_set_current_win(win)
+		return
+	end
+  
+  local expanded_path = expand_path(opts.target_file)
 
   if vim.fn.filereadable(expanded_path) == 0 then
     vim.notify("File not found: " .. expanded_path, vim.log.levels.ERROR)
+    return
   end
 
   local buf = vim.fn.bufnr(expanded_path, true)
@@ -36,7 +49,7 @@ local function open_floating_file(target_file)
 
   vim.bo[buf].swapfile = false
 
-  local win = vim.api.nvim_open_win(buf, true, win_config())
+  win = vim.api.nvim_open_win(buf, true, win_config(opts))
 
   vim.api.nvim_buf_set_keymap(buf, "n", "q", "", {
     noremap = true,
@@ -46,20 +59,21 @@ local function open_floating_file(target_file)
         vim.notify("Unsaved changes, close anyway?", vim.log.levels.WARN)
       else
         vim.api.nvim_win_close(0, true)
+        win = nil
       end
-    end
+    end,
   })
 end
 
-local function config_user_commands(opt)
-  local target_file = opt.target_file or "todo.md"
+local function config_user_commands(opts)
+  opts = vim.tbl_deep_extend("force", default_opts, opts)
   vim.api.nvim_create_user_command("td", function()
-    open_floating_file(target_file)
+    open_floating_file(opts)
   end, {})
 end
 
-M.config = function(opt)
-  config_user_commands(opt)
+M.config = function(opts)
+  config_user_commands(opts)
 end
 
 return M
